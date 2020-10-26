87d1fea9ca37b54e10ba39d46f3ea7ee3d77acca
repl.it user <replituser@example.com>
2020-10-26 17:10:56 +0000

    Removed lista_actividades

---
+++ tutorial.py
@@ -0,0 +1,19 @@
+# Python Tutorial
+
+# Tipos de datos
+ints = 10
+floats = 10.0
+strings = "Hola"
+booleans = True
+booleans = False
+tipo_none = None
+lista = [10, "hola", 10.32]
+print(lista)
+
+# Operaciones datos
+
+# Control de flujo
+
+# Loops
+
+# Funciones
