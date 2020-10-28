ccfeb3ac2ed6e31a7e252a06a9c3478edf0660ba
icmor <54549777+icmor@users.noreply.github.com>
2020-10-27 22:06:08 -0500

    Algoritmos de busqueda

---
+++ buscar.py
@@ -0,0 +1,22 @@
+""" Funciones de Búsqueda """
+
+
+def verify(func):
+    def wrapper(entry):
+        value = entry.get()
+        if value.isdigit():
+            func(int(value))
+        else:
+            popup("Por favor ingresa un número.")
+        entry.set("")
+    return wrapper
+
+
+@verify
+def linear_search(number):
+    pass
+
+
+@verify
+def binary_search(number):
+    pass
