934fc2b6567f1ac5e487d3300c603358913256fb
icmor <54549777+icmor@users.noreply.github.com>
2020-10-27 22:06:25 -0500

    Clase base de grafo

---
+++ grafos.py
@@ -0,0 +1,18 @@
+""" Algoritmos para Manipular Grafos """
+
+
+class Graph:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.graph = []
+
+    def add_edge(self, x, y, w):
+        """ Método para añadir un borde al grafo """
+        self.graph.append([x, y, w])
+
+    def kruskal(self):
+        """ Algoritmo de Kruskal para encontrar el árbol recubridor
+        mínimo """
+
+        # Árbol resultante
+        mst = []
