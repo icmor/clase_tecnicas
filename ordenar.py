f9bfb7d73432a20e67dc16d898eb07011bd88336
repl.it user <replituser@example.com>
2020-10-28 05:27:51 +0000

    Insertion Sort

--- ordenar.py
+++ ordenar.py
@@ -2,20 +2,40 @@
 
 from popup import popup
 import json
-
+import time
 
 def bubble_sort(numeros):
-    tam = len(numeros) 
-    for i in range(tam):
-        for j in range(tam-i-1):
+    start = time.perf_counter()
+    n = len(numeros)
+    for i in range(n):
+        for j in range(n-i-1):
             if numeros[j] > numeros[j+1]: 
                 numeros[j], numeros[j+1] = numeros[j+1], numeros[j]
 
+    total = round(time.perf_counter() - start, 2)
     with open("ordenados_burbuja.txt", "w") as file:
         json.dump(numeros, file)
+    popup(f"¡Los números han sido ordenados!\n\
+Tiempo de ejecución: {total}\n\
+Archivo con resultados: ordenados_burbuja.txt")
+
 
 def insertion_sort(numeros):
-    pass
+    start = time.perf_counter()
+    for i in range(1,len(numeros)):
+        j = i - 1
+        valor = numeros[i]
+        while j >= 0 and valor < numeros[j]:
+            numeros[j+1] = numeros[j]
+            j -= 1
+        numeros[j+1] = valor
+
+    total = round(time.perf_counter() - start, 2)
+    with open("ordenados_insercion.txt", "w") as file:
+        json.dump(numeros, file)
+    popup(f"¡Los números han sido ordenados!\n\
+Tiempo de ejecución: {total}\n\
+Archivo con resultados: ordenados_insercion.txt")
 
 
 def quicksort(numeros):
